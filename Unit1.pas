095b2637ceb6b63573dcad0e2f40d5b085994e9c
Kevin Kim Hirata <kevinhiratakaze@gmail.com>
2024-10-31 22:37:58 -0300

    Criptografia Hex em Delphi

---
+++ Unit1.pas
@@ -0,0 +1,45 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Edit1: TEdit;
+    Edit2: TEdit;
+    Button1: TButton;
+    Edit3: TEdit;
+    Edit4: TEdit;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  data: TDate;
+  serie: integer;
+begin
+  data := StrToDate(Edit1.Text);
+  serie := StrToInt(Edit3.Text);
+
+  Edit2.Text := IntToHex(Trunc(data + serie), 8);
+  Edit4.Text := IntToHex(Trunc(serie), 8);
+end;
+
+end.
